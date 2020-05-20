67a780163b2cc27390ab2babcddeef02a48bc177
ceosss <sswarajsamant@gmail.com>
2020-05-20 22:38:51 +0530

    created basic structure using react and firebase

--- src/App.js
+++ src/App.js
@@ -1,26 +1,106 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import firebase from "./firebase";
+
+import "./App.css";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      currentItem: "",
+      items: [],
+    };
+  }
+
+  componentDidMount = () => {
+    const itemsRef = firebase.database().ref("items");
+    itemsRef.on("value", (snapshot) => {
+      let items = snapshot.val();
+      let array = [];
+      for (let key in items) {
+        const item = {
+          id: key,
+          ...items[key],
+        };
+        array.push(item);
+      }
+      this.setState({ items: array });
+    });
+  };
+
+  removeItem = (id) => {
+    let itemRef = firebase.database().ref(`/items/${id}`);
+    itemRef.remove();
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const itemsRef = firebase.database().ref("items");
+    const item = {
+      title: this.state.currentItem,
+      user: this.state.username,
+    };
+    itemsRef.push(item);
+    this.setState({
+      currentItem: "",
+      username: "",
+    });
+  };
+
+  render() {
+    return (
+      <div className="app">
+        <header>
+          <div className="wrapper">
+            <h1>Fun Food Friends</h1>
+          </div>
+        </header>
+        <div className="container">
+          <section className="add-item">
+            <form onSubmit={this.handleSubmit}>
+              <input
+                type="text"
+                name="username"
+                placeholder="What's your name?"
+                value={this.state.username}
+                onChange={this.handleChange}
+              />
+              <input
+                type="text"
+                name="currentItem"
+                placeholder="What are you bringing?"
+                value={this.state.currentItem}
+                onChange={this.handleChange}
+              />
+              <button>Add Item</button>
+            </form>
+          </section>
+          <section className="display-item">
+            <div className="wrapper">
+              <ul>
+                {this.state.items.map((item, key) => (
+                  <li key={item.id}>
+                    <h3>{item.title}</h3>
+                    <p>brought by: {item.user}</p>
+                    <button onClick={() => this.removeItem(item.id)}>
+                      Remove Item
+                    </button>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </section>
+        </div>
+      </div>
+    );
+  }
 }
-
 export default App;
---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase";
+const config = {
+  apiKey: "AIzaSyAXr-ahwz2w5C9Mtx6h4lOHlypP3irIhn4",
+  authDomain: "pubg-tpurnament.firebaseapp.com",
+  databaseURL: "https://pubg-tpurnament.firebaseio.com",
+  projectId: "pubg-tpurnament",
+  storageBucket: "pubg-tpurnament.appspot.com",
+  messagingSenderId: "994058469613",
+  appId: "1:994058469613:web:67495e852ce470d1f78ce4",
+};
+firebase.initializeApp(config);
+export default firebase;
